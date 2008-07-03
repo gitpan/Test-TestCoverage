196d4dfd74452569921ebe915112399ade14626a
Renee Baecker <cpan@perl-services.de>
2008-07-03 06:28:07 +0000

    Import of RENEEB/Test-TestCoverage-0.07 from CPAN.
    gitpan-cpan-distribution: Test-TestCoverage
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         RENEEB/Test-TestCoverage-0.07.tar.gz
    gitpan-cpan-author:       RENEEB
    gitpan-cpan-maturity:     released

--- Test-TestCoverage-0.06/lib/Test/TestCoverage.pm
+++ Test-TestCoverage-0.06/lib/Test/TestCoverage.pm
@@ -16,7 +16,7 @@ our @EXPORT = qw(
                  reset_all_test_coverage
                  test_coverage_except
                 );
-our $VERSION = '0.07';
+our $VERSION = '0.06';
 
 my $self    = {};
 my $test    = Test::Builder->new();
